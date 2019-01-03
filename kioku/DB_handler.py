24621076e7240c44c959fca0d9b8bcc211fbdf71
romain <pelle.romain@gmail.com>
2019-01-03 15:49:10 +0100

    corrected select and count method unit tests properly done

--- kioku/DB_handler.py
+++ kioku/DB_handler.py
@@ -40,29 +40,56 @@ class DB_handler(metaclass=Singleton):
 			self.kiokuDB.close()
 			del(self.kiokuDB)
 
-	def select(self, selector, base, *itemToGet, **condition):
-		r = _req_select(selector, base, **condition)
-		if r == None :
+	# Public method ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''' 
+
+	def select(self, base, *itemToGet, **conditions):
+		if not self._check_select(base, *itemToGet, **conditions):
+			return None
+		data = self._req_select(base, *itemToGet, **conditions)
+		if data == None :
 			log.error("error selecting"+selector+" from base "+base)
 			return
-		cursor = self.kiokuDB.cursor()
-		data_to_fetch = [i for i,data in enumerate(base_format[base]) if data in itemToGet]
-		_data = [[row[i] for i in data_to_fetch] for row in cursor.fetchall()]
-		if len(data_to_fetch) ==1 : data = [item[0] for item in _data]
-		else : data = _data
 		return data
 
-	def count(self, base, **condition):
-		_req_select(None, base, condition)
-		cursor = self.kiokuDB.cursor()
-		return cursor.fetchone()[0]
+
+	def count(self, base, **conditions):
+		r = self._req_select(base, **conditions)
+		return r[0][0]
+
 
 	def add(self, base, *dataList):
-		for formatted_data in (self.format_entry(base, row) for row in dataList):
-			print(base)
-			print(formatted_data)
-			self._req_add(base, **formatted_data)
+
+		for data in dataList : 
+			if not self._check_entry(base, data):
+				log.error("failed adding to base "+base+" data : "+str(data))
+				return None
+			self._req_add(base, *data)
 		self.kiokuDB.commit()
+
+
+	# checkers ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+
+	def _check_entry(self, base, row) :
+		if not check_baseType(base): return False
+		expected_len = len(base_format[base])
+		given_len = len(row)
+		if expected_len != given_len: 
+			log.error("format not matching, expected: "+str(expected_len), "got: "+str(given_len))
+			return False
+		return True
+
+	def _check_select(self, base, *itemToGet, **conditions):
+		if not check_baseType(base): return False
+		shallExist = list(itemToGet) + list(conditions.keys())
+		missing = [item for item in shallExist if item not in base_format[base]]
+		if len(missing)>0 : 
+			log.error("missing row in base "+base+" :"+str(missing))
+			return False
+		return True
+
+	# executing request  ''''''''''''''''''''''''''''''''''''''''''''''''''''''
+	
 
 	def sqlR(func):
 		@functools.wraps(func)
@@ -71,46 +98,37 @@ class DB_handler(metaclass=Singleton):
 			if result == None:
 				return
 			cursor = self.kiokuDB.cursor()
-			if isinstance(result, tuple):
-				str_request, data_request = result
-				cursor.execute(str_request, data_request)
-			elif isinstance(result, str): 
-				cursor.execute(result)
-			return result
+			cursor.execute(result)
+			return cursor.fetchall()
 		return executing_R
 
-	@sqlR
-	def _req_select(self, selector, base, **condition):
-		if selector == "" : 
-			selector= "COUNT(*)"
-		sqlrequest = "SELECT " + selector + " FROM " + base
-		if condition :
-			sqlrequest += "WHERE "+condition.keys()[0] + "="+condition.values()[0]
-		return sqlrequest
 
 	@sqlR
-	def _req_add(self, base, **formatted_data):
-		print(base)
+	def _req_select(self, base, *itemToGet, **conditions):
+		
+		if itemToGet == () : 
+			selector= "COUNT(*)"
+		else : 
+			selector = ''
+			for item in itemToGet : selector+= item +', '
+			selector = selector[:-2]+' '
+		sqlrequest = "SELECT " + selector + " FROM " + base
+		if conditions :
+			sqlrequest += " WHERE "
+			for condition_id, condition_value in conditions.items() : 
+				sqlrequest += condition_id + "='"+condition_value + "' AND "
+			sqlrequest = sqlrequest[:-5]
+		return sqlrequest
+
+
+	@sqlR
+	def _req_add(self, base, *dataList):
 		if not check_baseType(base): return None
 		if base not in base_format.keys():
 			log.error("base :"+base+"not found.")
-		sqlrequest = "INSERT INTO "+base+str(base_format[base])+" VALUES "+str(tuple([":"+item for item in base_format[base]]))
-		print(sqlrequest)
-		return sqlrequest, formatted_data
+		sqlrequest = "INSERT INTO "+base+str(base_format[base])+" VALUES "+str(dataList)
+		return sqlrequest
 
-
-	def format_entry(self, base, row) :
-		if not check_baseType(base): return None
-		expected_len = len(base_format[base])
-		given_len = len(row)
-		if expected_len != given_len: 
-			log.error("format not matching, expected: "+str(expected_len), "got: "+given_len)
-			return None
-		formatted_data = {key : value for key, value in zip(base_format[base], row)}
-		return formatted_data
-
-
-
-
+		
 
 
--- test/test_DB_handler.py
+++ test/test_DB_handler.py
@@ -1,5 +1,5 @@
 #from .context.kioku.DB_handler import DB_handler
-import unittest, os
+import unittest, os, gc
 from shutil import copyfile
 from context import kioku
 from kioku.DB_handler import DB_handler
@@ -13,10 +13,11 @@ class TestDB_Handler(unittest.TestCase) :
 	def setUp(self):
 		copyfile(original_db, working_db)
 	def tearDown(self):
-		# os.remove(working_db)
+		gc.collect()
+		os.remove(working_db)
 		pass
 
-	def test_lol(self):
+	def test_dbHandler(self):
 		db_handler = DB_handler(working_db)
 		db_handler_2 = DB_handler(working_db)
 			
@@ -24,13 +25,25 @@ class TestDB_Handler(unittest.TestCase) :
 		self.assertEqual(id(db_handler), id(db_handler_2))
 
 		lol = [
-			("n_cat", "n_tag", "n_word", "n_hastuon", "n_meaning", "n_exemple")
+			("a_cat", "a_tag", "word_1", "hastuon_1", "a_meaning", "a_exemple"),
+			("a_cat", "c_tag", "word_2", "hastuon_2", "a_meaning", "a_exemple"),
+			("b_cat", "c_tag", "word_3", "hastuon_3", "b_meaning", "b_exemple"),		
 		]
 
 		db_handler.add("vocab",*lol)
+		a = db_handler.select("vocab", "word", "categorie")
+		b = db_handler.select("vocab", "word", categorie = "a_cat")
+		c = db_handler.select("vocab", "word", 'prononciation', categorie = "a_cat", tag = "c_tag")
+		
+		self.assertEqual(a, [('word_1', 'a_cat'), ('word_2', 'a_cat'), ('word_3', 'b_cat')])
+		self.assertEqual(b, [('word_1',), ('word_2',)])
+		self.assertEqual(c, [('word_2', 'hastuon_2')])
+
+		d = db_handler.count("vocab", categorie = "a_cat")
+		self.assertEqual(d, 2)
+
 
 		del(db_handler)
-		del(db_handler_2)
 
 if __name__ == '__main__':
     unittest.main()
